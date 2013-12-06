1d7bbe57e28f0818216d1fdc0cf0b8ef453f40f2
Stein Inge Morisbak <stein.inge.morisbak@BEKK.no>
2013-12-06 07:06:25 +0100

    disable bouncing applications in the dock

---
+++ manifests/dock/no_bouncing.pp
@@ -0,0 +1,13 @@
+# Public: Disables the dock by setting a long autohide-delay
+class osx::dock::no-bouncing {
+  include osx::dock
+
+  boxen::osx_defaults { 'Prevent bouncing applications in dock':
+    user   => $::boxen_user,
+    key    => 'no-bouncing',
+    type   => 'bool',
+    domain => 'com.apple.dock',
+    value  => 'true',
+    notify => Exec['killall Dock'];
+  }
+}
